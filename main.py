4d4d080f5051007d83483bd44ac30175ba539a32
Xayiel <izmyran@gmail.com>
2025-09-15 13:18:57 +0200

    added constants and starting on display

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import pygame
+from constants import *
 
 def main():
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
 
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    # Game loop, drawing game onto the screen
+    while True:
+        pygame.fill()
+        pygame.display.flip()
+
+    
+        
+                                     
+    
+    
 
 if __name__ == "__main__":
     main()
