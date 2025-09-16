c8c9fcf4d4c9f0163ce252ca5ae397cbd8fdf306
Xayiel <izmyran@gmail.com>
2025-09-16 16:17:21 +0200

    added triangle shape, and displaying it on screen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -11,14 +12,24 @@ def main():
     clock = pygame.time.Clock()
     dt = 0 
 
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
     # Game loop, drawing game onto the screen and capping at 60fps.
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
+        
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+
+        
+       
+
 
 if __name__ == "__main__":
     main()
