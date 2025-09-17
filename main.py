0c8c72dc277a7fd7ee6e74d3d4746764a6df44f5
Xayiel <izmyran@gmail.com>
2025-09-17 20:21:36 +0200

    added asteroids

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print("Starting Asteroids!")
@@ -15,8 +17,12 @@ def main():
     y = SCREEN_HEIGHT / 2
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
-    player = Player(x, y)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Player(x, y)
+    asteroid_field = AsteroidField()
 
 
     # Game loop, drawing game onto the screen and capping at 60fps.
